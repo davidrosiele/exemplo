2e58740d3d1661ca1b1314e31e5e8875afca799d
davidrosiele <143541797+davidrosiele@users.noreply.github.com>
2023-08-30 14:01:23 -0300

    Treino3

---
+++ src/medium/Treino3.java
@@ -0,0 +1,13 @@
+package medium;
+
+public class Treino3 {// inicio algoritimo
+    static String primeirovalor;
+    static String segundoValor;
+
+    public String retornaValorTrocado(String primeiroValor, String segundoValor) {
+        return "Primeiro valor digitado: " + segundoValor + "\n" +
+                "Segundo valor digitado: "+ primeirovalor;
+    }
+
+}//fim algoritimo
+
