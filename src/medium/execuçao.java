c37ee82d4318d3e36156544ae9689568adb1464d
davidrosiele <143541797+davidrosiele@users.noreply.github.com>
2023-08-30 13:08:25 -0300

    modificaçao

--- src/medium/execuçao.java
+++ src/medium/execuçao.java
@@ -4,10 +4,9 @@ import javax.swing.*;
 
 public class execuçao {
     public static void main(String[] args) {
-
-        String primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor ");
-        String segundoValor = JOptionPane.showInputDialog("Digite o segundo valor ");
-        exercicio exercicio = new exercicio();// instanciando a classe exercicio
-        System.out.println(exercicio.retornaValorTrocado(primeiroValor, segundoValor));//faço a chamada e valor
-    }
+        String numeroDigitadoStg = JOptionPane.showInputDialog("Por favor, digite um numero inteiro para calcula a tabuada:");
+       int numerDigitado = Integer.parseInt(numeroDigitadoStg);
+       Exercicio9 exercicio9 = new Exercicio9();
+       exercicio9.calculaTabuada(numerDigitado);
+        }
 }
